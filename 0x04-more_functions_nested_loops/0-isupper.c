95da6d90abae62216a6feaea0d8feca48a2eb50f
Wairura <mwekamotho@gmail.com>
2022-03-23 00:19:38 -0700

    nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,16 +7,16 @@
  */
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-    {
-      return (1);
-    }
-  else if (c <= 'z')
-    {
-      return (0);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else if (c <= 'z')
+{
+return (0);
+}
+else
+{
+return (0);
+}
 }
