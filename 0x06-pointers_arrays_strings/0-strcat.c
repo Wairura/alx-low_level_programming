a613edd2d79af181c253af2a6e9f87a24b6f278c
Wairura <mwekamotho@gmail.com>
2022-03-27 02:38:59 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,22 @@
-#include "holberton.h"
-/**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
- */
-char *_strcat(char *dest, char *src)
-{
-  int i;
-  int j;
+#include "main.h"
+#include <stdio.h>
 
-  i = 0;
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  j = 0;
-  while (src[j] != '\0')
-    {
-      dest[i] = src[j];
-      i++;
-      j++;
-    }
-  dest[i] = '\0';
-  return (dest);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char s1[98] = "Hello ";
+  char s2[] = "World!\n";
+  char *p;
+
+  printf("%s\n", s1);
+  printf("%s", s2);
+  p = _strcat(s1, s2);
+  printf("%s", s1);
+  printf("%s", s2);
+  printf("%s", p);
+  return (0);
 }
