f06f76f0f793b27ccc05cf959ef3db379693310f
Wairura <mwekamotho@gmail.com>
2022-03-29 06:52:42 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int i, j;
+int i, j;
 
-  i = j = 0;
-  while (*(dest + i))
-    i++;
-  while ((*(dest + i) = *(src + j)))
-    {
-      i++;
-      j++;
-    }
-  return (dest);
+i = j = 0;
+while (*(dest + i))
+i++;
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j++;
+}
+return (dest);
 }
